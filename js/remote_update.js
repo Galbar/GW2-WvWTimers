d6830ae16cf26b8cd4a4ad870ac10e75f3d976c9
Alessio Linares <alessio.linares2@gmail.com>
2013-06-25 13:47:07 +0200

    Finished update system

--- js/remote_update.js
+++ js/remote_update.js
@@ -1,4 +1,6 @@
-LatestVersion = "1.0.1";
+
+var LatestVersion = "1.0.1";
+console.log("Remote update");
 if (localStorage.version == undefined)
 {
 	requestUpdate();
@@ -19,53 +21,53 @@ function requestUpdate () {
 		"<p>One fine body…</p>"+
 		"</div>"+
 		"<div class='modal-footer'>"+
-		"<button class='btn' id='updateAlertClose' data-dismiss='modal' aria-hidden='true'></button>"+
-		"<button id='updateAlertAccept' class='btn btn-primary'></button>"+
+		"<button class='btn' id='updateAlertClose' data-dismiss='modal' aria-hidden='true'></button> "+
+		"<a href='http://build.phonegap.com/apps/440645/install/?qrkey=KisptxnCYUotJEmsyfPc'><button id='updateAlertAccept' class='btn btn-primary'></button></a>"+
 		"</div>"+
 		"</div>");
 
 	if(localStorage.lang == "en")
 	{
-		$("#myModalLabel").text("New Version Available");
-		$("#updateAlertClose").text("Close");
-		$("#updateAlertAccept").text("Download");
-		$(".modal-body").text("<strong> Version" + LatestVersion + ": List of changes: </ strong>" +
+		$("#myModalLabel").html("New Version Available");
+		$("#updateAlertClose").html("Close");
+		$("#updateAlertAccept").html("Download");
+		$(".modal-body").html("<strong>Version " + LatestVersion + ".<br>List of changes: </strong>" +
 			"<ul>" +
 			"<li> Added update alerts." +
-			"<li>" +
+			"</li>" +
 			"<ul>");
 	}
 	else if (localStorage.lang == "fr")
 	{
-		$("#myModalLabel").text("Nouvelle Version Disponible");
-		$("#updateAlertClose").text("Fermer");
-		$("#updateAlertAccept").text("Télécharger");
-		$(".modal-body").text("Version <strong>" + LatestVersion + ": Liste des modifications: </ strong>" +
+		$("#myModalLabel").html("Nouvelle Version Disponible");
+		$("#updateAlertClose").html("Fermer");
+		$("#updateAlertAccept").html("Télécharger");
+		$(".modal-body").html("<strong>Version " + LatestVersion + ".<br>Liste des modifications: </strong>" +
 			"<ul>" +
 			"<li> Ajouté alertes de mise à jour." +
-			"<li>" +
+			"</li>" +
 			"<ul>");
 	}
 	else if (localStorage.lang == "de")
 	{
-		$("#myModalLabel").text("Neue Version Verfügbar");
-		$("#updateAlertClose").text("Schließen");
-		$("#updateAlertAccept").text("Herunterladen");
-		$(".modal-body").text("<strong> Version" + LatestVersion + ": Liste der Änderungen: </ strong>" +
+		$("#myModalLabel").html("Neue Version Verfügbar");
+		$("#updateAlertClose").html("Schließen");
+		$("#updateAlertAccept").html("Herunterladen");
+		$(".modal-body").html("<strong>Version " + LatestVersion + ".<br>Liste der Änderungen: </strong>" +
 			"<ul>" +
 			"<li> Update Warnungen." +
-			"<li>" +
+			"</li>" +
 			"<ul>");
 	}
 	else if (localStorage.lang == "es")
 	{
-		$("#myModalLabel").text("Nueva Versión Disponible");
-		$("#updateAlertClose").text("Cerrar");
-		$("#updateAlertAccept").text("Descargar");
-		$(".modal-body").text("Versión <strong>" + LatestVersion + ": Lista de cambios: </ strong>" +
+		$("#myModalLabel").html("Nueva Versión Disponible");
+		$("#updateAlertClose").html("Cerrar");
+		$("#updateAlertAccept").html("Descargar");
+		$(".modal-body").html("<strong>Versión " + LatestVersion + ".<br>Lista de cambios: </strong>" +
 			"<ul>" +
 			"<li> Añadido alertas de actualización." +
-			"<li>" +
+			"</li>" +
 			"<ul>");
 	}
 
