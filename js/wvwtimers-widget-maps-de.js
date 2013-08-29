28ce734acb9007d3ddfafbd5429ad52a18ab5c09
Alessio Linares <alessio.linares2@gmail.com>
2013-08-30 00:51:21 +0200

    Super updategit checkout master  :DD Added map view mode

--- js/wvwtimers-widget-maps-de.js
+++ js/wvwtimers-widget-maps-de.js
@@ -1,71 +1,70 @@
 console.log("Entrando en el script");
-
 var viewing_match = "";
 var viewing_server = "";
 var viewing_map = "all";
 var hist_status = new Array();
 var objectives = new Array();
-			objectives[ 1 ] = ["Belvédère",25, 0];
-			objectives[ 2 ] = ["Vallée",25, 0];
-			objectives[ 3 ] = ["Basses terres",25, 0];
-			objectives[ 4 ] = ["Clairière de Golanta",5, 0];
-			objectives[ 5 ] = ["Montée de Pangloss",5, 0];
-			objectives[ 6 ] = ["Forêt rasée de Speldan",5, 0];
-			objectives[ 7 ] = ["Passage Danelon",5, 0];
-			objectives[ 8 ] = ["Bois d'Ombreclair",5, 0];
-			objectives[ 9 ] = ["Château Brumepierre",35, 0];
-			objectives[ 10 ] = ["Carrière des voleurs",5, 0];
-			objectives[ 11 ] = ["Corniche d'Aldon",10, 0];
-			objectives[ 12 ] = ["Piste du Ruisseau sauvage",10, 0];
-			objectives[ 13 ] = ["Bourbier de Jerrifer",10, 0];
-			objectives[ 14 ] = ["Petit ravin de Klovan",10, 0];
-			objectives[ 15 ] = ["Ravin de Langor",10, 0];
-			objectives[ 16 ] = ["Lac Quentin",10, 0];
-			objectives[ 17 ] = ["Faille de Mendon",10, 0];
-			objectives[ 18 ] = ["Col d'Anzalias",10, 0];
-			objectives[ 19 ] = ["Percée de Gardogre",10, 0];
-			objectives[ 20 ] = ["Flanc de Veloka",10, 0];
-			objectives[ 21 ] = ["Ravin de Durios",10, 0];
-			objectives[ 22 ] = ["Falaise de Bravost",10, 0];
-			objectives[ 23 ] = ["Garnison",25, 0];
-			objectives[ 24 ] = ["Fief du champion",5, 0];
-			objectives[ 25 ] = ["Bruyerouge",10, 0];
-			objectives[ 26 ] = ["Lac Vert",10, 0];
-			objectives[ 27 ] = ["Baie de l'Ascension",25, 0];
-			objectives[ 28 ] = ["Promontoire de l'aube",10, 0];
-			objectives[ 29 ] = ["L'antre des esprits",5, 0];
-			objectives[ 30 ] = ["Gentesylve",10, 0];
-			objectives[ 31 ] = ["Collines d'Askalion",25, 0];
-			objectives[ 32 ] = ["Collines d'Etheron",25, 0];
-			objectives[ 33 ] = ["Baie des rêves",25, 0];
-			objectives[ 34 ] = ["Pavillon du vainqueur",5, 0];
-			objectives[ 35 ] = ["Vertebranche",10, 0];
-			objectives[ 36 ] = ["Lac bleu",10, 0];
-			objectives[ 37 ] = ["Garnison",25, 0];
-			objectives[ 38 ] = ["Longuevue",10, 0];
-			objectives[ 39 ] = ["L'Epée divine",5, 0];
-			objectives[ 40 ] = ["Flanc de falaise",10, 0];
-			objectives[ 41 ] = ["Collines de Shadaran",25, 0];
-			objectives[ 42 ] = ["Rougelac",10, 0];
-			objectives[ 43 ] = ["Pavillon du Héros",5, 0];
-			objectives[ 44 ] = ["Baie du Noir déclin",25, 0];
-			objectives[ 45 ] = ["Bruyazur",10, 0];
-			objectives[ 46 ] = ["Garnison",25, 0];
-			objectives[ 47 ] = ["Colline ensoleillée",10, 0];
-			objectives[ 48 ] = ["Ferveur",5, 0];
-			objectives[ 49 ] = ["Refuge de bleuval",5, 0];
-			objectives[ 50 ] = ["Basses terres d'Eau-Azur",5, 0];
-			objectives[ 51 ] = ["Astralholme",5, 0];
-			objectives[ 52 ] = ["Espoir d'Arah",5, 0];
-			objectives[ 53 ] = ["Refuge de Valvert",5, 0];
-			objectives[ 54 ] = ["Havre gris",5, 0];
-			objectives[ 55 ] = ["Basses terres de Rubicon",5, 0];
-			objectives[ 56 ] = ["Bras du titan",5, 0];
-			objectives[ 57 ] = ["Sommet de l'escarpement",10, 0];
-			objectives[ 58 ] = ["Divination",5, 0];
-			objectives[ 59 ] = ["Refuge de Valrouge",5, 0];
-			objectives[ 60 ] = ["Bosquet stellaire",5, 0];
-			objectives[ 61 ] = ["Basses terres d'Eau-Verdoyante",5, 0];
+			objectives[ 1 ] = ["Aussichtspunkt",25, 0, 130, 270];
+			objectives[ 2 ] = ["Tal",25, 0, 366, 392];
+			objectives[ 3 ] = ["Tiefland",25, 0, 360, 90];
+			objectives[ 4 ] = ["Golanta-Lichtung",5, 0, 420, 180];
+			objectives[ 5 ] = ["Pangloss-Anhöhe",5, 0, 140, 355];
+			objectives[ 6 ] = ["Speldan Kahlschlag",5, 0, 115, 118];
+			objectives[ 7 ] = ["Danelon-Passage",5, 0, 436, 308];
+			objectives[ 8 ] = ["Umberlichtung-Forst",5, 0, 250, 408];
+			objectives[ 9 ] = ["Schloss Steinnebel",35, 0, 276, 247];
+			objectives[ 10 ] = ["Schurkenbruch",5, 0, 250, 80];
+			objectives[ 11 ] = ["Aldons Vorsprung",10, 0, 314, 54];
+			objectives[ 12 ] = ["Wildbachstrecke",10, 0, 280, 134];
+			objectives[ 13 ] = ["Jerrifers Sumpfloch",10, 0, 408, 122];
+			objectives[ 14 ] = ["Klovan-Senke",10, 0, 357, 173];
+			objectives[ 15 ] = ["Langor - Schlucht",10, 0, 425, 380];
+			objectives[ 16 ] = ["Quentinsee",10, 0, 380, 288];
+			objectives[ 17 ] = ["Mendons Spalt",10, 0, 100, 190];
+			objectives[ 18 ] = ["Anzalias-Pass",10, 0, 188, 183];
+			objectives[ 19 ] = ["Ogerwacht-Kanal",10, 0, 165, 315];
+			objectives[ 20 ] = ["Veloka-Hang",10, 0, 100, 315];
+			objectives[ 21 ] = ["Durios-Schlucht",10, 0, 265, 340];
+			objectives[ 22 ] = ["Bravost-Abhang",10, 0, 312, 426];
+			objectives[ 23 ] = ["Festung",25, 0, 280, 230];
+			objectives[ 24 ] = ["Landgut des Champions",5, 0, 586, 233];
+			objectives[ 25 ] = ["Rotdornstrauch",10, 0, 458, 156];
+			objectives[ 26 ] = ["Grünsee",10, 0, 468, 332];
+			objectives[ 27 ] = ["Bucht des Aufstiegs",25, 0, 383, 31];
+			objectives[ 28 ] = ["Horst der Morgendammerung",10, 0, 209, 351];
+			objectives[ 29 ] = ["Der Geisterholm",5, 0, 46, 235];
+			objectives[ 30 ] = ["Wald - Freistatt",10, 0, 208, 109];
+			objectives[ 31 ] = ["Askalion - Hügel",25, 0, 368, 462];
+			objectives[ 32 ] = ["Etheron - Hügel",25, 0, 368, 462];
+			objectives[ 33 ] = ["Traumbucht",25, 0, 383, 31];
+			objectives[ 34 ] = ["Sieger - Hütte",5, 0, 586, 233];
+			objectives[ 35 ] = ["Grünstrauch",10, 0, 458, 156];
+			objectives[ 36 ] = ["Blausee",10, 0, 468, 332];
+			objectives[ 37 ] = ["Festung",25, 0, 280, 230];
+			objectives[ 38 ] = ["Weitsicht",10, 0, 208, 109];
+			objectives[ 39 ] = ["Das Gottschwert",5, 0, 46, 235];
+			objectives[ 40 ] = ["Felswand",10, 0, 209, 351];
+			objectives[ 41 ] = ["Shadaran Hügel",25, 0, 368, 462];
+			objectives[ 42 ] = ["Rotsee",10, 0, 468, 332];
+			objectives[ 43 ] = ["Hütte des Helden",5, 0, 586, 233];
+			objectives[ 44 ] = ["Schreckensfall - Bucht",25, 0, 383, 31];
+			objectives[ 45 ] = ["Blaudornstrauch",10, 0, 458, 156];
+			objectives[ 46 ] = ["Festung",25, 0, 280, 230];
+			objectives[ 47 ] = ["Sonnenlichthügel",10, 0, 208, 109];
+			objectives[ 48 ] = ["Glaubenssprung",5, 0, 236, 64];
+			objectives[ 49 ] = ["Blautal - Zuflucht",5, 0, 480, 70];
+			objectives[ 50 ] = ["Blauwasser - Tiefland",5, 0, 490, 420];
+			objectives[ 51 ] = ["Astralholm",5, 0, 228, 422];
+			objectives[ 52 ] = ["Arahs Hoffnung",5, 0, 236, 64];
+			objectives[ 53 ] = ["Grüntal - Zuflucht",5, 0, 480, 70];
+			objectives[ 54 ] = ["Nebel - Freistatt",5, 0, 228, 422];
+			objectives[ 55 ] = ["Rotwasser - Tiefland",5, 0, 490, 420];
+			objectives[ 56 ] = ["Die Titanenpranke",5, 0, 46, 235];
+			objectives[ 57 ] = ["Felsenspitze",10, 0, 209, 351];
+			objectives[ 58 ] = ["Götterkunde",5, 0, 236, 64];
+			objectives[ 59 ] = ["Rottal - Zuflucht",5, 0, 480, 70];
+			objectives[ 60 ] = ["Sternenhain",5, 0, 228, 422];
+			objectives[ 61 ] = ["Grünwasser - Tiefland",5, 0, 490, 420];
 
 function getWorldName(id, world_names) {
 	var i = 0;
@@ -123,7 +122,6 @@ function getTimeLeft(time)
 	var m=Math.floor( time/(60000) );
 	var s=Math.floor( (time/1000)-(m*60) );
 	// add a zero in front of numbers<10
-	m=checkTime(m);
 	s=checkTime(s);
 	return m+" : "+s;
 }
@@ -164,9 +162,11 @@ function getMatchCallBack()
 {
 	return function (status)
 	{
+		var firstTime = false;
 		if (hist_status.length == 0)
 		{
 			hist_status = status;
+			firstTime = true;
 //			console.log("inicializando hist_status");
 		}
 		if ($(".WvWTimers-widget").attr("mode") < "2")
@@ -187,26 +187,22 @@ function getMatchCallBack()
 			{
 				if ($(".WvWTimers-widget").attr("mode") > "0")
 				{
-					if(status.maps[k].objectives[j].owner != hist_status.maps[k].objectives[j].owner)
+					if(status.maps[k].objectives[j].owner != hist_status.maps[k].objectives[j].owner || firstTime)
 					{
-						if (objectives[status.maps[k].objectives[j].id][2] != 0)
+						if(firstTime)
 						{
-							$("#entry"+status.maps[k].objectives[j].id).remove();
+							var obj = "<div class=\"map-structure-container\" style=\"top: "+objectives[status.maps[k].objectives[j].id][3]+"px;left:"+objectives[status.maps[k].objectives[j].id][4]+"px;\">"+
+									  "<i data-toggle=\"tooltip\" title=\""+objectives[status.maps[k].objectives[j].id][0]+"\" id=\"icon"+status.maps[k].objectives[j].id+"\" class=\"obj-icon objective-"+status.maps[k].objectives[j].owner+" "+getObjectiveType(status.maps[k].objectives[j].id)+"\"></i>"+
+									  "<span id=\"timer"+status.maps[k].objectives[j].id+"\" class=\"label\"></span>"+
+									  "</div>";
+							$(".map-"+status.maps[k].type).append(obj);
 						}
-						$("#log").append(
-							"<tr id='entry"+status.maps[k].objectives[j].id+"' class='map-all map-"+status.maps[k].type+"'>"+
-							"<td>"+
-							"<i class='objective-"+status.maps[k].objectives[j].owner+" "+getObjectiveType(status.maps[k].objectives[j].id)+"'></i><strong class='"+getTeamColor(status.maps[k].objectives[j].owner)+"'>"+objectives[status.maps[k].objectives[j].id][0]+"</strong>"+
-							"</td>"+
-							"<td>"+
-							"<span id='timer"+status.maps[k].objectives[j].id+"' class='label "+getLableClass(status.maps[k].objectives[j].owner)+"'></span>"+
-							"</td>"+
-							"</tr>");
-						if (viewing_map != "all" && status.maps[k].type != viewing_map)
-						{
-							$(".map-"+status.maps[k].type).css("display", "none");
-						}
-						objectives[status.maps[k].objectives[j].id][2] = new Date();
+
+						$("#icon"+status.maps[k].objectives[j].id).removeClass("objective-"+hist_status.maps[k].objectives[j].owner);
+						$("#icon"+status.maps[k].objectives[j].id).addClass("objective-"+status.maps[k].objectives[j].owner);
+
+						if(!firstTime)
+							objectives[status.maps[k].objectives[j].id][2] = new Date();
 						hist_status.maps[k].objectives[j] = status.maps[k].objectives[j];
 					}
 				}
@@ -234,6 +230,10 @@ function getMatchCallBack()
 			$("#"+"id"+viewing_match+"barteam0").css("width", (status.scores[0]/max_score*100)+"%");
 			$("#"+"id"+viewing_match+"barteam0").html("+"+red);
 		}
+		if (firstTime)
+		{
+			$(".obj-icon").tooltip();
+		};
 	};
 }
 
@@ -252,11 +252,12 @@ function updateTimers()
 			if (now.getTime() >= objectives[i][2].getTime()+300000)
 			{
 				objectives[i][2] = 0;
-				$("#entry"+i).remove();
+				$("#timer"+i).css("display", "none");
 			}
 			else
 			{
 				$("#timer"+i).html(getTimeLeft(now - objectives[i][2]));
+				$("#timer"+i).css("display", "block");
 			}
 		}
 	};
@@ -279,50 +280,42 @@ if ($(".WvWTimers-widget").attr("mode") > "0")
 	$(".WvWTimers-widget").append(
 	"<div id='WvW-widget-log'>"+
 	"<ul class='tabs-mobile nav-maps nav-tabs'>"+
-	"<li class='map-view-all active' id='all'>"+
-	"	<a href='#'>Tous</a>"+
-	"</li>"+
-	"<li class='map-view'  id='Center'>"+
-	"	<a href='#' class='NoTeamColor'>Éternels</a>"+
+	"<li class='map-view active'  id='Center'>"+
+	"	<a href='#' class='NoTeamColor'>Ewige</a>"+
 	"</li>"+
 	"<li class='map-view'  id='GreenHome'>"+
-	"	<a href='#' class='GreenTeamColor'>Vert</a>"+
+	"	<a href='#' class='GreenTeamColor'>Grüne</a>"+
 	"</li>"+
 	"<li class='map-view'  id='BlueHome'>"+
-	"	<a href='#' class='BlueTeamColor'>Bleu</a>"+
+	"	<a href='#' class='BlueTeamColor'>Blaue</a>"+
 	"</li>"+
 	"<li class='map-view'  id='RedHome'>"+
-	"	<a href='#' class='RedTeamColor'>Rouge</a>"+
+	"	<a href='#' class='RedTeamColor'>Rotem</a>"+
 	"</li>"+
 	"</ul>"+
 	"<ul class='tabs-desktop nav-maps nav-tabs'>"+
-	"<li class='map-view-all active' id='all'>"+
-	"	<a href='#'>Tous</a>"+
-	"</li>"+
-	"<li class='map-view'  id='Center'>"+
-	"	<a href='#' class='NoTeamColor'>Champs de Bataille Éternels</a>"+
+	"<li class='map-view active'  id='Center'>"+
+	"	<a href='#' class='NoTeamColor'>Ewige Schlachtfelder</a>"+
 	"</li>"+
 	"<li class='map-view'  id='GreenHome'>"+
-	"	<a href='#' class='GreenTeamColor'>Frontières Verts</a>"+
+	"	<a href='#' class='GreenTeamColor'>Grenzlande Grüne</a>"+
 	"</li>"+
 	"<li class='map-view'  id='BlueHome'>"+
-	"	<a href='#' class='BlueTeamColor'>Frontières Bleu</a>"+
+	"	<a href='#' class='BlueTeamColor'>Grenzlande Blaue</a>"+
 	"</li>"+
 	"<li class='map-view'  id='RedHome'>"+
-	"	<a href='#' class='RedTeamColor'>Frontières Rouge</a>"+
+	"	<a href='#' class='RedTeamColor'>Grenzlande Rotem</a>"+
 	"</li>"+
 	"</ul>"+
-	"<table class='table'>"+
-	"<thead id='loghead'>"+
-	"<tr>"+
-	"<th><span class='text'>Objectif</span></th>"+
-	"<th><span class='text'>Temps</span></th>"+
-	"</tr>"+
-	"</thead>"+
-	"</table>"+
-	"<div id='table-scroll'>"+
-	"<table id='log' class='table'>"+
-	"</table>"+
+	"<div id='map-scroll'>"+
+	"<div id='eternal-map' class='map-Center map-container'>"+
+	"</div>"+
+	"<div id='green-map' class='map-GreenHome map-container'>"+
+	"</div>"+
+	"<div id='blue-map' class='map-BlueHome map-container'>"+
+	"</div>"+
+	"<div id='red-map' class='map-RedHome map-container'>"+
+	"</div>"+
 	"</div>"+
 	"</div>");
 }
@@ -330,26 +323,19 @@ if ($(".WvWTimers-widget").attr("mode") > "0")
 if(localStorage.show_scores == "false")
   $("#WvW-widget-log").css('top', 0 );
 
-$(".WvWTimers-widget").append("<span id='footer'>Par <a href='https://github.com/Galbar/' target='_blank'>Galbar</a></span>");
+$(".WvWTimers-widget").append("<span id='footer'>By <a href='https://github.com/Galbar/' target='_blank'>Galbar</a></span>");
 
 viewing_server = $(".WvWTimers-widget").attr("server-id");
 console.log("viewing_server: "+viewing_server);
 	$(".map-view").click(function(){
-		$(".map-view-all").removeClass("active");
 		$(".map-view").removeClass("active");
 		$(this).addClass("active");
-		$(".map-all").css("display", "none");
-		$(".map-"+$(this).attr("id")).css("display", "");
-		viewing_map = $(this).attr("id");
-	});
-	$(".map-view-all").click(function(){
-		$(".map-view").removeClass("active");
-		$(this).addClass("active");
-		$(".map-all").css("display", "");
+		$(".map-container").css("display", "none");
+		$(".map-"+$(this).attr("id")).css("display", "block");
 		viewing_map = $(this).attr("id");
 	});
 	console.log("getting ready");
-	$.getJSON("https://api.guildwars2.com/v1/world_names.json?lang=fr", function (world_names) {
+	$.getJSON("https://api.guildwars2.com/v1/world_names.json?", function (world_names) {
 		$.getJSON("https://api.guildwars2.com/v1/wvw/matches.json?", function (aux_matches) {
 			var matches = aux_matches.wvw_matches;
 			var found = false;
